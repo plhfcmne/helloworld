8cf76212087c32fbcd6402fffeb26916ea3287b4
My Name is Universe <alyonushkagirl22@mail.ru>
2022-12-19 22:09:21 +0300

    поменяли смотрим что там ещепоменялось

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,3 @@
 public class Main {public static void main(String[] args) {
-        System.out.println("Привет, мир");}}
+        System.out.println("тренируем еще одну замену");
+    System.out.println("ghjdthztv ");}}
